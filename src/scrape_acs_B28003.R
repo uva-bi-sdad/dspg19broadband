bb3f5ff682f78e0cfddd6dd4ee7a1cc7d17e080b
sm9dv <sm9dv@virginia.edu>
2019-06-24 20:56:10 +0000

    updated for b28003

--- src/scrape_acs_B28003.R
+++ src/scrape_acs_B28003.R
@@ -1,4 +1,4 @@
-scrape ACS data
+#scrape ACS data
 library(tidycensus)
 library(acs)
 library(tigris)
@@ -9,12 +9,11 @@ library(readr)
 
 census_api_key("548d39e0315b591a0e9f5a8d9d6c1f22ea8fafe0")
 
-#PRESENCE AND TYPES OF INTERNET SUBSCRIPTIONS IN HOUSEHOLD
+#PRESENCE COMPUTER AND TYPES OF INTERNET SUBSCRIPTIONS IN HOUSEHOLD
 my_states <- unique(fips_codes$state)[1:51]
 my_counties <- fips_codes %>% filter(state %in% my_states)
-my_variables_B28002 <- c("B28002_001", "B28002_002", "B28002_003", "B28002_004",
-                         "B28002_005", "B28002_006", "B28002_007", "B28002_008", "B28002_009",
-                         "B28002_010", "B28002_011", "B28002_012", "B28002_013")
+my_variables_B28003 <- c("B28003_001", "B28003_002", "B28003_003", "B28003_004",
+                         "B28003_005", "B28003_006")
 
 county_state <- tigris::counties(
   state = my_states,
@@ -24,12 +23,12 @@ county_state <- tigris::counties(
   class = "sf"
 )
 
-internet_sub_bbdata_list <- list()
+computer_internet_sub_bbdata_list <- list()
 for(i in 1:3142){
   try(
-    internet_sub_bbdata_list[[i]] <- get_acs(
+    computer_internet_sub_bbdata_list[[i]] <- get_acs(
       geography = "block group",
-      variables = my_variables_B28002 ,
+      variables = my_variables_B28003 ,
       state = county_state$STATEFP[i],
       county = county_state$COUNTYFP[i],
       year = 2017,
@@ -40,13 +39,13 @@ for(i in 1:3142){
 }
 
 # get index of counties for which the API call failed
-ind_failed <- which( sapply(internet_sub_bbdata_list,is.null) )
+ind_failed <- which( sapply(computer_internet_sub_bbdata_list,is.null) )
 # retry
 for(i in ind_failed){
   try(
-    internet_sub_bbdata_list[[i]] <- get_acs(
+    computer_internet_sub_bbdata_list[[i]] <- get_acs(
       geography = "block group",
-      variables = my_variables_B28002 ,
+      variables = my_variables_B28003 ,
       state = county_state$STATEFP[i],
       county = county_state$COUNTYFP[i],
       year = 2017,
@@ -56,6 +55,6 @@ for(i in ind_failed){
   )
 }
 
-internet_sub_bbdata <- rbindlist(internet_sub_bbdata_list)
+computer_internet_sub_bbdata <- rbindlist(computer_internet_sub_bbdata_list)
 
-# write_csv(internet_sub_bbdata,"~/../sdad/project_data/usda/dspg2019broadband/working/acs5yr/acsB28002.csv")
+write_csv(computer_internet_sub_bbdata,"~/dspg19broadband/data/working/acs5yr/acsB28003.csv")
