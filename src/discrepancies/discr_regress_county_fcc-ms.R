2d84f694e1affba6d472ebbcda681b02dbd463e8
Teja Pristavec <tp2sk@virginia.edu>
2019-09-20 14:30:11 +0000

    Added rural-urban analyses

--- src/discrepancies/discr_regress_county_fcc-ms.R
+++ src/discrepancies/discr_regress_county_fcc-ms.R
@@ -9,7 +9,6 @@ library(naniar)
 library(gridExtra)
 library(ggplot2)
 library(gbm)
-library(xgboost)
 
 
 #
@@ -97,17 +96,32 @@ hist(discr$dis_rel_fcc_ms)
 # Select data
 data <- discr %>% select(dis_rel_fcc_ms, RUCC_2013, hs_r_ls, poverty, ag_65_l, hispanc, black, density, family, foreign)
 
+data_rural <- discr %>%
+              filter(ru_binary == "nonmetro") %>%
+              select(dis_rel_fcc_ms, RUCC_2013, hs_r_ls, poverty, ag_65_l, hispanc, black, density, family, foreign)
+data_urban <- discr %>%
+              filter(ru_binary == "metro") %>%
+              select(dis_rel_fcc_ms, RUCC_2013, hs_r_ls, poverty, ag_65_l, hispanc, black, density, family, foreign)
+
 # Set seed
 set.seed(2410)
 
 # Split
-split  <- initial_split(data, prop = 0.7)
+split <- initial_split(data, prop = 0.7)
 data_train <- training(split)
 data_test <- testing(split)
 
+split_urban <- initial_split(data_urban, prop = 0.7)
+data_train_urban <- training(split_urban)
+data_test_urban <- testing(split_urban)
+
+split_rural <- initial_split(data_rural, prop = 0.7)
+data_train_rural <- training(split_rural)
+data_test_rural <- testing(split_rural)
+
 
 #
-# Random forest regression ---------------------------------------------------------------------------
+# Random forest regression: ALL ---------------------------------------------------------------------------
 #
 
 # Sample model
@@ -204,6 +218,208 @@ ggplot(data = comparison, aes(x = dis_rel_fcc_ms, y = preds)) +
   geom_abline(intercept = 0, slope = 1, colour = "red") +
   coord_cartesian(xlim = c(0, 100), ylim = c(0, 100)) +
   labs(title = "Actual versus predicted predicted FCC-Microsoft discrepancy values (%)", x = "Actual FCC-Microsoft discrepancy (%)", 
+       y = "Predicted FCC-Microsoft discrepancy (%)")
+
+
+#
+# Random forest regression: URBAN ---------------------------------------------------------------------------
+#
+
+# Sample model
+n_features <- length(setdiff(names(data_train_urban), "dis_rel_fcc_ms"))
+
+discr_m1 <- ranger(
+  dis_rel_fcc_ms ~ ., 
+  data = data_train_urban,
+  num.trees = n_features * 10, # Number of trees needs to be sufficiently large to stabilize error rate. Rule of thumb = start with 10 x the number of features, but adjust to other hyperparameters (mtry, node size). More trees = more robust/stable error estimates and variable importance measures + more computational time. 
+  mtry = floor(n_features / 3), # Controls the split-variable randomization feature. Helps balance low tree correlation with reasonable predictive strength. With regression problems the default value is often mtry=(p/3) and for classification mtry=sqrt(p) (p = number of features). When there are fewer relevant predictors, a higher mtry value performs better because it makes it more likely to select those features with the strongest signal. When there are many relevant predictors, a lower mtry might perfrom better.
+  min.node.size = 5, # When adjusting node size start with three values between 1–10 and adjust depending on impact to accuracy and run time. Regression default is 5. 
+  replace = TRUE, # If you have inbalanced categorical features try sampling without replacement.
+  sample.fraction = 1, # Decreasing the sample size leads to more diverse trees and thereby lower between-tree correlation, which can have a positive effect on the prediction accuracy. If there are a few dominating features in your data set, reducing the sample size can also help to minimize between-tree correlation. Assess 3–4 values of sample sizes ranging from 25%–100%.
+  seed = 2410
+)
+
+discr_m1
+default_rmse <- sqrt(discr_m1$prediction.error) # OOB RMSE
+
+# Grid search setup
+hyper_grid <- expand.grid(
+  num.trees = n_features * c(8, 9, 10, 11, 12),           # number of trees
+  mtry = floor(n_features * c(.05, .15, .25, .333, .4)),  # split rule
+  min.node.size = c(1, 3, 5, 7, 10),                      # tree complexity
+  replace = c(TRUE, FALSE),                               # sampling scheme
+  sample.fraction = c(.5, .63, .8, 1),                    # sampling scheme
+  rmse = NA                                               # results placeholder
+)
+
+# Full cartesian grid search
+for(i in seq_len(nrow(hyper_grid))) {
+  # fit model for ith hyperparameter combination
+  fit <- ranger(
+    formula         = dis_rel_fcc_ms ~ ., 
+    data            = data_train_urban, 
+    num.trees       = hyper_grid$num.trees[i],
+    mtry            = hyper_grid$mtry[i],
+    min.node.size   = hyper_grid$min.node.size[i],
+    replace         = hyper_grid$replace[i],
+    sample.fraction = hyper_grid$sample.fraction[i],
+    verbose         = FALSE,
+    seed            = 2410,
+  )
+  # export OOB error 
+  hyper_grid$rmse[i] <- sqrt(fit$prediction.error)
+}
+
+# View top 10 performing models and consider gain from default model
+hyper_grid %>%
+  arrange(rmse) %>%
+  mutate(perc_gain = (default_rmse - rmse) / default_rmse * 100) %>%
+  head(10)
+
+# Variable importance from best model
+discr_m1perm <- ranger(
+  dis_rel_fcc_ms ~ ., 
+  data = data_train_urban,
+  num.trees = 72, 
+  mtry = 1, 
+  min.node.size = 3, 
+  replace = TRUE, 
+  sample.fraction = 0.50, 
+  importance = "permutation",
+  seed = 2410
+)
+
+discr_m1imp <- ranger(
+  dis_rel_fcc_ms ~ ., 
+  data = data_train_urban,
+  num.trees = 72, 
+  mtry = 1, 
+  min.node.size = 3, 
+  replace = TRUE, 
+  sample.fraction = 0.50, 
+  importance = "impurity",
+  seed = 2410
+)
+
+p1 <- vip::vip(discr_m1perm, bar = FALSE) + ggtitle("Impurity-based")
+p2 <- vip::vip(discr_m1imp, bar = FALSE) + ggtitle("Permutation-based")
+gridExtra::grid.arrange(p1, p2, nrow = 1)
+
+# Predict
+preds <- predict(discr_m1perm, data_test_urban)
+preds <- data.frame(preds$predictions)
+
+comparison <- cbind(dis_rel_fcc_ms = data_test_urban$dis_rel_fcc_ms, preds = preds$preds.predictions)
+comparison <- as.data.frame(comparison)
+head(comparison)
+
+# Plot actual versus predicted FCC-Microsoft discrepancy (in %)
+ggplot(data = comparison, aes(x = dis_rel_fcc_ms, y = preds)) + 
+  geom_point() + 
+  geom_abline(intercept = 0, slope = 1, colour = "red") +
+  coord_cartesian(xlim = c(0, 100), ylim = c(0, 100)) +
+  labs(title = "Actual versus predicted predicted FCC-Microsoft discrepancy values (%) [urban counties]", x = "Actual FCC-Microsoft discrepancy (%)", 
+       y = "Predicted FCC-Microsoft discrepancy (%)")
+
+
+#
+# Random forest regression: RURAL---------------------------------------------------------------------------
+#
+
+# Sample model
+n_features <- length(setdiff(names(data_train_rural), "dis_rel_fcc_ms"))
+
+discr_m1 <- ranger(
+  dis_rel_fcc_ms ~ ., 
+  data = data_train_rural,
+  num.trees = n_features * 10, # Number of trees needs to be sufficiently large to stabilize error rate. Rule of thumb = start with 10 x the number of features, but adjust to other hyperparameters (mtry, node size). More trees = more robust/stable error estimates and variable importance measures + more computational time. 
+  mtry = floor(n_features / 3), # Controls the split-variable randomization feature. Helps balance low tree correlation with reasonable predictive strength. With regression problems the default value is often mtry=(p/3) and for classification mtry=sqrt(p) (p = number of features). When there are fewer relevant predictors, a higher mtry value performs better because it makes it more likely to select those features with the strongest signal. When there are many relevant predictors, a lower mtry might perfrom better.
+  min.node.size = 5, # When adjusting node size start with three values between 1–10 and adjust depending on impact to accuracy and run time. Regression default is 5. 
+  replace = TRUE, # If you have inbalanced categorical features try sampling without replacement.
+  sample.fraction = 1, # Decreasing the sample size leads to more diverse trees and thereby lower between-tree correlation, which can have a positive effect on the prediction accuracy. If there are a few dominating features in your data set, reducing the sample size can also help to minimize between-tree correlation. Assess 3–4 values of sample sizes ranging from 25%–100%.
+  seed = 2410
+)
+
+discr_m1
+default_rmse <- sqrt(discr_m1$prediction.error) # OOB RMSE
+
+# Grid search setup
+hyper_grid <- expand.grid(
+  num.trees = n_features * c(8, 9, 10, 11, 12),           # number of trees
+  mtry = floor(n_features * c(.05, .15, .25, .333, .4)),  # split rule
+  min.node.size = c(1, 3, 5, 7, 10),                      # tree complexity
+  replace = c(TRUE, FALSE),                               # sampling scheme
+  sample.fraction = c(.5, .63, .8, 1),                    # sampling scheme
+  rmse = NA                                               # results placeholder
+)
+
+# Full cartesian grid search
+for(i in seq_len(nrow(hyper_grid))) {
+  # fit model for ith hyperparameter combination
+  fit <- ranger(
+    formula         = dis_rel_fcc_ms ~ ., 
+    data            = data_train_rural, 
+    num.trees       = hyper_grid$num.trees[i],
+    mtry            = hyper_grid$mtry[i],
+    min.node.size   = hyper_grid$min.node.size[i],
+    replace         = hyper_grid$replace[i],
+    sample.fraction = hyper_grid$sample.fraction[i],
+    verbose         = FALSE,
+    seed            = 2410,
+  )
+  # export OOB error 
+  hyper_grid$rmse[i] <- sqrt(fit$prediction.error)
+}
+
+# View top 10 performing models and consider gain from default model
+hyper_grid %>%
+  arrange(rmse) %>%
+  mutate(perc_gain = (default_rmse - rmse) / default_rmse * 100) %>%
+  head(10)
+
+# Variable importance from best model
+discr_m1perm <- ranger(
+  dis_rel_fcc_ms ~ ., 
+  data = data_train_rural,
+  num.trees = 90, 
+  mtry = 2, 
+  min.node.size = 5, 
+  replace = TRUE, 
+  sample.fraction = 1, 
+  importance = "permutation",
+  seed = 2410
+)
+
+discr_m1imp <- ranger(
+  dis_rel_fcc_ms ~ ., 
+  data = data_train_rural,
+  num.trees = 90, 
+  mtry = 2, 
+  min.node.size = 5, 
+  replace = TRUE, 
+  sample.fraction = 1, 
+  importance = "impurity",
+  seed = 2410
+)
+
+p1 <- vip::vip(discr_m1perm, bar = FALSE) + ggtitle("Impurity-based")
+p2 <- vip::vip(discr_m1imp, bar = FALSE) + ggtitle("Permutation-based")
+gridExtra::grid.arrange(p1, p2, nrow = 1)
+
+# Predict
+preds <- predict(discr_m1perm, data_test_rural)
+preds <- data.frame(preds$predictions)
+
+comparison <- cbind(dis_rel_fcc_ms = data_test_rural$dis_rel_fcc_ms, preds = preds$preds.predictions)
+comparison <- as.data.frame(comparison)
+head(comparison)
+
+# Plot actual versus predicted FCC-Microsoft discrepancy (in %)
+ggplot(data = comparison, aes(x = dis_rel_fcc_ms, y = preds)) + 
+  geom_point() + 
+  geom_abline(intercept = 0, slope = 1, colour = "red") +
+  coord_cartesian(xlim = c(0, 100), ylim = c(0, 100)) +
+  labs(title = "Actual versus predicted predicted FCC-Microsoft discrepancy values (%) [rural counties]", x = "Actual FCC-Microsoft discrepancy (%)", 
        y = "Predicted FCC-Microsoft discrepancy (%)")
 
 
